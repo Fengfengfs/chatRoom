37225d406cf9f71988f96eaa517facebe171c9a6
陈铭杰 <chetchen@yunify.com>
2022-01-19 20:20:04 +0800

    feat(实时视频): 完善功能逻辑和ui界面

--- server/socket.js
+++ server/socket.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io')
 const httpServer = http.createServer()
 const ioServer = new Server(httpServer)
 const SOCKETPORT = process.env.SOCKETPORT || 7002
-const { CONNECT, CREATEROOM, LOGIN, MESSAGE, RECEIVEUSERLIST, LOGOUT, VEDIO_OFFER, VEDIO_ANSWER, VEDIO_CANDIDATE } = require('./config/ioSocket')
+const { CONNECT, CREATEROOM, LOGIN, MESSAGE, VEDIO_CLOSE, RECEIVEUSERLIST, LOGOUT, VEDIO_OFFER, VEDIO_ANSWER, VEDIO_CANDIDATE } = require('./config/ioSocket')
 const resopnse = require('./utils/res')
 // 创建连接 
 const roomsMap = new Map()
@@ -12,10 +12,10 @@ const commonRoomsMap = new Map()
 const OnlyRoom = 'OnlyRoom'
 const getAllUsers = (name, socket) => {
   roomsMap.has(name) || roomsMap.set(name, { name, id: socket.id, rooms: [], socket: socket, role: 'user' })
-  debugger
+
   const allusers = [...roomsMap.values()].filter(user => user.role == 'user').map(ele => ele.name)
   const allrooms = roomsMap.get(name).rooms
-  debugger
+
   return [allusers, allrooms]
 }
 ioServer.on(CONNECT, function (socket) {
@@ -23,38 +23,40 @@ ioServer.on(CONNECT, function (socket) {
   //监听用户登陆信息
   socket.on(LOGIN, function (user) {
     // 用户登陆
-    debugger
+
     roomsMap.set(user.username, { name: user.username, id: socket.id, rooms: [], socket, role: 'user' })
     socket.to(OnlyRoom).emit(LOGIN, resopnse.success({ username: user.username }))
   })
   socket.on(LOGOUT, function (username) {
     // 用户退出
     roomsMap.delete(username)
-    debugger
+
     socket.to(OnlyRoom).emit(LOGOUT, username)
   })
   // 监听消息发送
   socket.on(MESSAGE, function (data) {
     // 消息通知
-    debugger
+
     let { fromRoom, msg, toRoom, username } = data
     const currentRoom = roomsMap.get(toRoom)
     const roomId = currentRoom?.id || OnlyRoom
     if (currentRoom.role == 'commonRoom') {
       fromRoom = toRoom
     }
-    debugger
+
     socket.to(roomId).emit(MESSAGE, resopnse.success({ fromRoom: fromRoom, msg, toRoom: toRoom, username }))
   })
   socket.on(RECEIVEUSERLIST, function (name) {
     // 获取房间列表
+    if (!name) return
+
     socket.emit(RECEIVEUSERLIST, resopnse.success(getAllUsers(name, socket)))
   })
   socket.on(CREATEROOM, (list) => {
     const date = new Date().valueOf()
-    debugger
+
     let commonRoomName = list.slice(0, 2).reduce((str, target) => {
-      str += target
+      str += `和${target}`
       return str
     }, '')
     list.length > 2 && (commonRoomName += '...')
@@ -65,10 +67,11 @@ ioServer.on(CONNECT, function (socket) {
       debugger
       roomsMap.set(name, {
         ...user,
-        rooms: [commonRoomName, ...user.rooms]
+        rooms: [commonRoomName, ...user?.rooms]
       })
       if (user.role != 'commonRoom') {
         user.socket.join(commonRoomName)
+
         user.socket.emit(RECEIVEUSERLIST, resopnse.success(getAllUsers(name, user.socket)))
       }
     });
@@ -78,24 +81,24 @@ ioServer.on(CONNECT, function (socket) {
   socket.on(VEDIO_OFFER, (msg) => {
     const data = JSON.parse(msg)
     const { target } = data
-    debugger
+
     const roomId = roomsMap.get(target)?.id
     if (roomId) {
       socket.to(roomId).emit(VEDIO_OFFER, msg)
     }
   })
   socket.on(VEDIO_ANSWER, (msg) => {
-    debugger
+
     const data = JSON.parse(msg)
     const { target } = data
-    debugger
+
     const roomId = roomsMap.get(target)?.id
     if (roomId) {
       socket.to(roomId).emit(VEDIO_ANSWER, msg)
     }
   })
   socket.on(VEDIO_CANDIDATE, (msg) => {
-    debugger
+
     const data = JSON.parse(msg)
     const { target, candidate } = data
     console.log(target)
@@ -104,9 +107,18 @@ ioServer.on(CONNECT, function (socket) {
       socket.to(roomId).emit(VEDIO_CANDIDATE, msg)
     }
   })
+  socket.on(VEDIO_CLOSE, (msg) => {
+    const data = JSON.parse(msg)
+    const { target } = data
+
+    const roomId = roomsMap.get(target)?.id
+    if (roomId) {
+      socket.to(roomId).emit(VEDIO_CLOSE, msg)
+    }
+  })
 })
 
 httpServer.listen(7002, function () {
-  debugger
+
   console.log('服务启动成功!端口：', SOCKETPORT)
 })
